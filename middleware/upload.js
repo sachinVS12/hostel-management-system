248841ccda8c34d4a028cf64efbc57436a021c04
Sachin V S <vs1sachi12@gmail.com>
2026-04-01 16:10:00 +0530

    middleware

---
+++ middleware/upload.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed"));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5000000 }, // 5MB
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
